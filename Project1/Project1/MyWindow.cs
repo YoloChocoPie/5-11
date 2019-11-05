666e733a5a1b21f182cede7213ea348e8e59e761
YoloChocoPie <chocopieyolo@gmail.com>
2019-11-05 15:24:36 +0700

    update 1

---
+++ Project1/Project1/MyWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    class Mywindow : Form
+    {
+        static void Main()
+        {
+            Mywindow form = new Mywindow();
+            form.Text = " My First Time";
+
+            Button button = new Button();
+            button.Text = " click me ";
+            form.Controls.Add(button);
+
+            Application.Run(form);
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+            this.Load += Form1_Load;
+            this.button1.Click += button1_Click;
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aaaaaa");
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("helloooo");
+        }
+
+        void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //e.Cancel = true; ko xoa
+            MessageBox.Show("dont go :( ");
+
+        }
+    }
+}
